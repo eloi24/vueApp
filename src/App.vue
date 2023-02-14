07356807a36b6c940c458fef3fe13b6ebdd37205
Eloi Baulenas <eloi@javajan.com>
2023-02-14 16:35:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  
+  <nav class="navbar">
+    <img src="./assets/logo.svg" width="50">
+    <div class="brand">Todo List App</div>
+  </nav>
+  <main class="container">
+    <section>
+      <form class="add-todo-form" >
+        <input v-model="todoTitle"  type="text" placeholder="Todo Title">
+        <div>
+          <button @click.prevent="addTodo">Add Todo</button>
+        </div>
+      </form>
+
+    </section>
+    <section>
+      <div class="todo">
+        <p>Pasear al perro</p>
+        <div>
+          <button class="remove-todo-btn">&times;</button>
+        </div>
+      </div>
+
+    </section>
+  </main>
+</template>
+
+<script>
+const component ={
+  data(){
+    return {
+      todoTitle: "",
+      todos: [],
+    }
+  },
+
+  methods: {
+    updateTitle(e){
+      this.todoTitle =e.target.value;
+      console.log(this.todoTitle)
+    },
+    addTodo(e){
+      e.preventDefault();
+      this.todos.push(this.todoTitle)
+    }
+
+  }
+}
+
+export default component;
+</script>
+
+
+
+<style scoped>
+
+.navbar{
+  display: flex;
+  background: var(--navbar-color);
+  align-items: center;
+  padding: 20px;
+  margin-bottom: 30px;
+}
+
+.brand{
+  font-size: 2rem;
+}
+
+.add-todo-form{
+  display: flex;
+  justify-content: space-between;
+}
+
+.add-todo-form input{
+  width: 80%;
+  border: solid 2px var(--accent-color);
+}
+
+.add-todo-form button{
+  background-color: var(--accent-color);
+  color: var(--text-color);
+  border: none;
+  height: 50px;
+}
+
+.todo{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--accent-color);
+  margin-top: 30px;
+  padding: 0 20px 0 20px;
+  border-radius: 10px;
+}
+
+.remove-todo-btn{
+  border-radius: 50%;
+  border: none;
+  height: 40px;
+  width: 40px;
+  font-size: 30px;
+  color: var(--text-color);
+  background: var(--danger-color);
+}
+</style>
